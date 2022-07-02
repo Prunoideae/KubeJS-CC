e1ee077e7b39b5f4060e6029d251d06b40d4a175
Prunoideae <a455167189@163.com>
2022-07-02 18:58:16 +0800

    removed annoying info

--- forge/src/main/java/com/prunoideae/kjscc/forge/ForgePeripheralProvider.java
+++ forge/src/main/java/com/prunoideae/kjscc/forge/ForgePeripheralProvider.java
@@ -5,7 +5,6 @@ import com.prunoideae.kjscc.PeripheralJS;
 import com.prunoideae.kjscc.PeripheralProviderBase;
 import dan200.computercraft.api.peripheral.IPeripheral;
 import dan200.computercraft.api.peripheral.IPeripheralProvider;
-import dev.latvian.mods.kubejs.KubeJS;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.Level;
@@ -23,8 +22,6 @@ public class ForgePeripheralProvider extends PeripheralProviderBase implements I
     @Override
     public LazyOptional<IPeripheral> getPeripheral(@NotNull Level world, @NotNull BlockPos pos, @NotNull Direction side) {
         PeripheralJS peripheral = getPeripheralJS(world.getBlockState(pos)).orElse(null);
-        KubeJS.LOGGER.info(pos.toString());
-        KubeJS.LOGGER.info(peripherals.toString());
         if (peripheral != null)
             return LazyOptional.of(() -> new DynamicPeripheralJS(peripheral.getType(), world, pos, side, peripheral.getMethods()));
         else
